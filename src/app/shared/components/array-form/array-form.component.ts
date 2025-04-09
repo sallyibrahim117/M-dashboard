cc5ff70d737817b4e22445b1eac50e6f9ecfdc20
sallyibrahim117@gmail.com <sallyibrahim117@gmail.com>
2025-04-09 05:42:10 +0200

    edit

--- src/app/shared/components/array-form/array-form.component.ts
+++ src/app/shared/components/array-form/array-form.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { log } from 'console';
 import { sign } from 'crypto';
 import { DropdownModule } from 'primeng/dropdown';
 @Component({
@@ -18,28 +19,32 @@ export class ArrayFormComponent {
  constructor(private fb:FormBuilder) {
 
    this.myForm=this.fb.group({
-
-     skills:this.fb.array([])
+    name:this.fb.control('',[Validators.required]),
+    email:this.fb.control('',[Validators.required]),
+    skills:this.fb.array([])
    })
   }
-    skills():FormArray{
+    skillsArray():FormArray{
       return this.myForm.get("skills") as FormArray
     }
+
     addSkill(){
-const skill =this.fb.group({
-  skill:new FormControl(''),
-   exp:new FormControl('')
+      const skill =this.fb.group({
+        skill:this.fb.control('',[Validators.required]),
+        exp:new FormControl('',[Validators.required])
 
-})
-this.skills().push(skill)
+      })
+      this.skillsArray().push(skill)
     }
+
     deleteSkill(index:number){
-      this.skills().removeAt(index)
+      this.skillsArray().removeAt(index)
     }
-    save(){
-console.log(this.myForm.value);
 
+    save(){
+     console.log(this.myForm.value);
     }
+
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
@@ -49,5 +54,12 @@ console.log(this.myForm.value);
     this.skillsList.set([{id:1,name:"html"},{id:1,name:"css"},
       {id:1,name:"js"},
     ])
+
+    this.addSkill();
+
+    this.myForm.valueChanges.subscribe((res:any)=>
+    {
+      console.log(res);
+    })
   }
 }
